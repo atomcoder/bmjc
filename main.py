9dfbb945a09b12ee0dd1a7b49a91258f89b2a4e6
Atom <sjfhades@gmail.com>
2016-09-21 23:55:45 +0800

    update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# coding = utf-8
+
 import tkinter
 
 app = tkinter.Tk()
--- regedit-netInterface.py
+++ regedit-netInterface.py
@@ -1,4 +1,9 @@
+# coding = utf-8
+
 import winreg
+import datetime
+
+TimedeltaPYtoMS = datetime.datetime(1970, 1, 1, 0, 0) - datetime.datetime(1601, 1, 1, 0, 0)
 
 Key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, 'SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces')
 KeyValueList = []
@@ -7,11 +12,15 @@ try:
     for i in range(winreg.QueryInfoKey(Key)[0]):
         SubKeyName = winreg.EnumKey(Key, i)
         SubKey = winreg.OpenKey(Key, SubKeyName)
+        SubKeyTime = datetime.datetime.fromtimestamp((winreg.QueryInfoKey(SubKey)[2]/10000000) -
+                                                     TimedeltaPYtoMS.total_seconds())  # Time
+        print(SubKeyTime)
         SubKeyValueList = list()
         for j in range(winreg.QueryInfoKey(SubKey)[1]):
             SubKeyValue = winreg.EnumValue(SubKey, j)
             SubKeyValueList.append(SubKeyValue)
         SubKeyValueList = sorted(SubKeyValueList)
+        SubKeyValueList.insert(0, ('Time', SubKeyTime.strftime('%Y-%m-%d,%H:%M:%S')))
         SubKeyValueList.insert(0, ('Interface', SubKeyName))
         KeyValueList.append(SubKeyValueList)
         SubKey.Close()
