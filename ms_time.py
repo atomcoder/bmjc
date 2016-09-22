1de5eb769f52a7729aa10ed837d678e285909c06
atomcoder <sjfhades@gmail.com>
2016-09-22 11:34:39 +0800

    update

---
+++ ms_time.py
@@ -0,0 +1,12 @@
+# coding = utf-8
+import datetime
+
+
+def ms_datetime(ms_datetime_var):
+    return datetime.datetime.fromtimestamp((ms_datetime_var / 10000000) -
+                                           (datetime.datetime(1970, 1, 1, 0, 0) - datetime.datetime(1601, 1, 1, 0, 0))
+                                           .total_seconds())  # MSTime to PythonTime
+if __name__ == '__main__':
+    print('MSTime is since Jan,1,1601.\nPythonTime is since Jan,1,1970.')
+    print('In RegEdit the key was last modified (if available) as 100’s of nanoseconds since Jan 1, 1601')
+    print('Now is', datetime.datetime.now())
--- regedit-netInterface.py
+++ regedit-netInterface.py
@@ -1,9 +1,8 @@
 # coding = utf-8
 
 import winreg
-import datetime
+import ms_time
 
-TimedeltaPYtoMS = datetime.datetime(1970, 1, 1, 0, 0) - datetime.datetime(1601, 1, 1, 0, 0)
 
 Key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, 'SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces')
 KeyValueList = []
@@ -12,8 +11,7 @@ try:
     for i in range(winreg.QueryInfoKey(Key)[0]):
         SubKeyName = winreg.EnumKey(Key, i)
         SubKey = winreg.OpenKey(Key, SubKeyName)
-        SubKeyTime = datetime.datetime.fromtimestamp((winreg.QueryInfoKey(SubKey)[2]/10000000) -
-                                                     TimedeltaPYtoMS.total_seconds())  # Time
+        SubKeyTime = ms_time.ms_datetime(winreg.QueryInfoKey(SubKey)[2])
         SubKeyValueList = list()
         for j in range(winreg.QueryInfoKey(SubKey)[1]):
             SubKeyValue = winreg.EnumValue(SubKey, j)
@@ -26,5 +24,6 @@ try:
 except WindowsError:
     print()
 Key.Close()
-for i in KeyValueList:
-    print(i)
+if __name__ == "__main__":
+    for i in KeyValueList:
+        print(i)
