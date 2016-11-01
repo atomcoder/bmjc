ea8604021444626c8c707c114ac7d252754c7f88
Atom <sjfhades@gmail.com>
2016-11-02 01:37:27 +0800

    update

--- get_system_info.py
+++ get_system_info.py
@@ -6,9 +6,9 @@ import psutil
 import datetime
 
 
-boot_time = datetime.datetime.fromtimestamp(psutil.boot_time()) #开机时间
-cpu_count = psutil.cpu_count() #CPU数量
-cpu_percent = psutil.cpu_percent(interval=0.1, percpu=True)#当前CPU占用率
+boot_time = datetime.datetime.fromtimestamp(psutil.boot_time())  # 开机时间
+cpu_count = psutil.cpu_count()  # CPU数量
+cpu_percent = psutil.cpu_percent(interval=0.1, percpu=True) # 当前CPU占用率
 
 
 print(boot_time, cpu_count, cpu_percent)
--- main.py
+++ main.py
@@ -4,5 +4,10 @@ import tkinter
 import list_net_interface
 
 app = tkinter.Tk()
+app.title('Tools by Atom')
+app.geometry('800x600')
+
+label1 = tkinter.Label(app, text=list_net_interface.list_net_interface())
+label1.pack()
 
 app.mainloop()
